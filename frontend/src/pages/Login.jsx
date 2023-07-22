380b380dfe0a01364d058a9f33cdc3fb10f4876a
IT21181856 <it21181856@my.sliit.lk>
2023-07-22 17:22:42 +0530

    FE ui

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import { FaSignInAlt } from 'react-icons/fa'
+
+function Login() {
+    const  [formData, setFormData]=useState({   //an object and that object have several attributes name, email, pw, pw2
+                                                  //default values are empty strings
+        email:'',
+        password:''
+    })
+
+    const { email,password}=formData   //This line uses object destructuring to extract the individual attributes (name, email, password, and password2) from the formData object. After this line, you can use these variables directly instead of accessing them through formData.name, formData.email, etc.
+                                                            //The line const {name, email, password, password2} = formData; is like taking values from a box and giving them individual names.
+                                                            //Imagine formData is a box, and inside that box, we have four smaller boxes named name, email, password, and password2. Each of these smaller boxes contains some information (initially empty strings).
+                                                            //With the line const {name, email, password, password2} = formData;, we open the big box (formData) and take out the contents from the four smaller boxes and assign them to four new variables, which are simply named name, email, password, and password2. Now you can use these new variables directly to access the values inside them, like name, email, etc., instead of writing formData.name, formData.email, etc. It's a more convenient way to work with the data inside the formData object.
+     const onChange = (e) =>{
+        setFormData((prevState)=>({
+            ...prevState,
+            [e.target.name]:e.target.value,       //name is the name of the input field ex: name='email'  , name='name'
+        }))
+     }
+
+     const onSubmit = (e) =>{
+        e.preventDefault()
+     }
+    
+     return (
+    <>
+    <section className="heading">
+        <h1>
+            <FaSignInAlt/> Login
+        </h1>
+        <p>Login and start setting goals</p>
+    </section>
+
+    <section className="form">
+        <form onSubmit={onSubmit}>
+            
+
+            <div className="form-group">
+            <input type="email" className="form-control" id='email'  name='email' value={email} 
+            placeholder='Eneter your email' onChange={onChange}/>
+            </div>
+
+            <div className="form-group">
+            <input type="password" className="form-control" id='password'  name='password' value={password} 
+            placeholder='Enter your password' onChange={onChange}/>
+            </div>
+
+
+            <div className="form-group">
+                <button type="submit" className='btn btn-block'>Submit</button>
+            </div>
+            
+        </form>
+    </section>
+    </>
+  )
+}
+
+export default Login
